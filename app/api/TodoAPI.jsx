6401374b0b5dc585de1cb675d93509076dcbb089
Renan <renandf@gmail.com>
2016-06-19 14:37:34 +1000

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -14,9 +14,36 @@ module.exports = {
 		try {
 			todos = JSON.parse(stringTodos);
 		} catch (e) {
-			
+
 		}
 
 		return $.isArray(todos) ? todos : [];
+	},
+	filterTodos: function (todos, showCompleted, searchText) {
+		var filteredTodos = todos;
+
+		// Filter by showCompleted
+		filteredTodos = filteredTodos.filter((todo) => {
+			return !todo.completed || showCompleted;
+		});
+
+		// Filter by searchText
+		filteredTodos = filteredTodos.filter((todo) => {
+			var text = todo.text.toLowerCase();
+			return searchText.length === 0 || text.indexOf(searchText) > -1;
+		});
+
+		// Sort todos with non-completed first
+		filteredTodos.sort((a, b) => {
+			if (a.completed && b.completed) {
+				return -1;
+			} else if (a.completed && !b.completed) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+
+		return filteredTodos;
 	}
 };
